371313646a2fb088ec4235d6cc6bbe83a52408fc
Burkhanov Ildar <burhanoff1992@gmail.com>
2020-09-29 03:50:07 +0300

    dsadad

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
     const body            = $('body'),
-          burger          = $('.header-burger')
-          overlay         = $('.overlay, .customize-form-close')
+          burger          = $('.header-burger'),
+          overlay         = $('.overlay'),
+          headerMain      = $('.header'),
+          headerOffset    = headerMain.innerHeight(),
+          headerNavItem   = $('.header-nav-drop')
+
+    headerNavItem.hover(
+        function() {
+            $(this).addClass('hover')
+        },
+        function () {
+            $(this).removeClass('hover')
+        }
+    )
 
     burger.click(function () {
         body.toggleClass('state-nav')
@@ -10,4 +22,33 @@ $(document).ready(function() {
     overlay.on("click", function () {
         body.removeClass("state-nav")
     })
+
+    const responsiveHooks = function (getWidth) {
+        // fixed header
+        if ( getWidth >= 991 ) {
+            $(window).scroll(function () {
+                var st = $(this).scrollTop()
+
+                if (st >= headerOffset) {
+                    $('.header-fixed-placeholder').css({
+                        height: headerOffset + 'px'
+                    })
+                    headerMain.addClass('header-fixed')
+                } else {
+                    $('.header-fixed-placeholder').css({
+                        height: 0
+                    })
+                    headerMain.removeClass('header-fixed')
+                }
+            })
+        }
+    }
+
+    $(window).resize(function () {
+        var currentWidth = $(this).innerWidth();
+
+        responsiveHooks(currentWidth)
+    });
+
+    responsiveHooks($(this).innerWidth());
 })
